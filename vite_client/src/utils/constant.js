9377daf5412e512cfd0831c6cdddb75347af3d5c
Ntish Choudhary <nitishchoudhary15e@gmail.com>
2025-10-17 00:24:07 +0530

    g

--- vite_client/src/utils/constant.js
+++ vite_client/src/utils/constant.js
@@ -1,17 +1,19 @@
 //==================Admin===================//
-export const ADMIN_AUTH_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/auth";
-export const ADMIN_LEAVE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/leave";
-export const ADMIN_PROJECT_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/project";
-export const ADMIN_PAYROLL_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/admin/payroll";
- 
+const BASE = process.env.VITE_API_URL;
+//==================Admin===================//
+export const ADMIN_AUTH_ENDPOINT = `${BASE}/api/v1/admin/auth`;
+export const ADMIN_LEAVE_ENDPOINT = `${BASE}/api/v1/admin/leave`;
+export const ADMIN_PROJECT_ENDPOINT = `${BASE}/api/v1/admin/project`;
+export const ADMIN_PAYROLL_ENDPOINT = `${BASE}/api/v1/admin/payroll`;
+
 //==================Employee===================//
-export const EMPLOYEE_AUTH_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/employee/auth";
-export const EMPLOYEE_LEAVE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/employee/leave";
- 
+export const EMPLOYEE_AUTH_ENDPOINT = `${BASE}/api/v1/employee/auth`;
+export const EMPLOYEE_LEAVE_ENDPOINT = `${BASE}/api/v1/employee/leave`;
+
 //==================Both===================//
-export const BOTH_PROFILE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/profile-details";
-export const BOTH_NOTIFICATION_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/notification";
-export const BOTH_ATTENDANCE_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/attendance";
-export const BOTH_DOCUMENT_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/document";
-export const BOTH_PASSWORD_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/password";
-export const BOTH_TASK_ENDPOINT = "https://server-f8bw.onrender.com/api/v1/both/project-task";
+export const BOTH_PROFILE_ENDPOINT = `${BASE}/api/v1/both/profile-details`;
+export const BOTH_NOTIFICATION_ENDPOINT = `${BASE}/api/v1/both/notification`;
+export const BOTH_ATTENDANCE_ENDPOINT = `${BASE}/api/v1/both/attendance`;
+export const BOTH_DOCUMENT_ENDPOINT = `${BASE}/api/v1/both/document`;
+export const BOTH_PASSWORD_ENDPOINT = `${BASE}/api/v1/both/password`;
+export const BOTH_TASK_ENDPOINT = `${BASE}/api/v1/both/project-task`;
